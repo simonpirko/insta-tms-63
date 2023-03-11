ce9b0d2fdb7a4bf570d4e7320574ce69e574f395
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-11 22:44:20 +0300

    Added method findUserByUsername

--- src/main/java/by/tms/insta/storage/SQLUserStorage.java
+++ src/main/java/by/tms/insta/storage/SQLUserStorage.java
@@ -3,6 +3,8 @@ package by.tms.insta.storage;
 import by.tms.insta.entity.User;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class SQLUserStorage implements UserStorage {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
@@ -14,8 +16,8 @@ public class SQLUserStorage implements UserStorage {
     private static final int FULL_NAME_COLUMN = 4;
     private static final int CREATE_AT_COLUMN = 5;
     private static final int UPDATE_AT_COLUMN = 6;
-    private static final String INSERT_USER = "insert into users values (default, ?, ?, ?, ?, ?, ?)";
-
+    private static final String INSERTING_USER = "insert into users values (default, ?, ?, ?, ?, ?, ?)";
+    private static final String SELECTION_USER_BY_USERNAME = "select * from users where username = ?";
     private final Connection connection;
 
     public SQLUserStorage() {
@@ -29,7 +31,7 @@ public class SQLUserStorage implements UserStorage {
     @Override
     public void save(User user) {
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER);
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERTING_USER);
             preparedStatement.setString(USERNAME_COLUMN, user.getUsername());
             preparedStatement.setString(PASSWORD_COLUMN, user.getPassword());
             preparedStatement.setString(EMAIL_COLUMN, user.getEmail());
@@ -40,5 +42,25 @@ public class SQLUserStorage implements UserStorage {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public Optional<User> findUserByUsername(String username) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECTION_USER_BY_USERNAME);
+            preparedStatement.setString(USERNAME_COLUMN, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+            String password = resultSet.getString(PASSWORD_COLUMN);
+            String email = resultSet.getString(EMAIL_COLUMN);
+            String fullName = resultSet.getString(FULL_NAME_COLUMN);
+            LocalDateTime createAt = resultSet.getTimestamp(CREATE_AT_COLUMN).toLocalDateTime();
+            LocalDateTime updateAt = resultSet.getTimestamp(UPDATE_AT_COLUMN).toLocalDateTime();
+
+            return Optional.of(new User(username, password, email, fullName, createAt, updateAt));
+        } catch (SQLException ignored) {
+        }
+        return Optional.empty();
     }
 }
--- src/main/java/by/tms/insta/storage/UserStorage.java
+++ src/main/java/by/tms/insta/storage/UserStorage.java
@@ -2,6 +2,9 @@ package by.tms.insta.storage;
 
 import by.tms.insta.entity.User;
 
+import java.util.Optional;
+
 public interface UserStorage {
     void save(User user);
+    Optional<User> findUserByUsername(String username);
 }

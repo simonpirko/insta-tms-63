62fd38b37657c3f2fc7f4cccc1712c7881260334
JuryDadian <filejob@mail.ru>
2023-03-13 00:04:26 +0300

    INST-14 add doPost method to AuthServlet

---
+++ src/main/java/by/tms/insta/servlet/AuthServlet.java
@@ -0,0 +1,37 @@
+package by.tms.insta.servlet;
+
+import by.tms.insta.entity.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet("/auth")
+public class AuthServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        Optional<User> byUsername = userService.findByUser(username);
+        if (byUsername.isPresent()) {
+            User user = byUsername.get();
+            if (user.getPassword().equals(password)) {
+                req.getSession().setAttribute("user", user);
+                req.setAttribute("username", username);
+                req.setAttribute("password", password);
+                resp.sendRedirect("/");
+                return;
+            } else {
+                req.setAttribute("message", "Wrong password!");
+            }
+        } else {
+            req.setAttribute("message", "User not found!");
+        }
+        getServletContext().getRequestDispatcher("/auth.jsp").forward(req, resp);
+    }
+}

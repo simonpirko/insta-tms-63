647dd94c4cc30f944980688b1f63040eecdacc3c
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-13 11:47:39 +0300

    Added a singleton pattern into the class UserService

---
+++ src/main/java/by/tms/insta/service/UserService.java
@@ -0,0 +1,15 @@
+package by.tms.insta.service;
+
+public class UserService {
+    private static UserService userService;
+
+    private UserService() {
+    }
+
+    public static UserService getInstance() {
+        if (userService == null) {
+            userService = new UserService();
+        }
+        return userService;
+    }
+}

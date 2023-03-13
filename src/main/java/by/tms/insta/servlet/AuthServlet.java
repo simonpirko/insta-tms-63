891d1f271cf6761023d8bf4e8d7a049c86c8adfd
Deniselop <volejkodenis302@gmail.com>
2023-03-13 17:07:08 +0300

    add WebServlet annotation

--- src/main/java/by/tms/insta/servlet/AuthServlet.java
+++ src/main/java/by/tms/insta/servlet/AuthServlet.java
@@ -4,11 +4,14 @@ package by.tms.insta.servlet;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@WebServlet("/auth")
 public class AuthServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)

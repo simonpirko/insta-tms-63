3c8996520128be4e3ff0ec402fc101b159f970f7
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-13 11:31:18 +0300

    Updated Storage

--- src/main/java/by/tms/insta/storage/Storage.java
+++ src/main/java/by/tms/insta/storage/Storage.java
@@ -5,7 +5,7 @@ import by.tms.insta.entity.User;
 import java.util.Optional;
 
 public abstract class Storage<T> {
-    abstract void save(T value);
-    abstract void remove(long id);
-    abstract Optional<User> findUserByUsername(String username);
+    public abstract void save(T value);
+    public abstract void remove(long id);
+    public abstract Optional<User> findUserByUsername(String username);
 }

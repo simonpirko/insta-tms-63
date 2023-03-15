ad0c0e5ace763f4632af1b22a2b35366a52131d6
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-15 23:16:15 +0300

    Added a method remove into the class JDBCPostStorage

--- src/main/java/by/tms/insta/storage/JDBCPostStorage.java
+++ src/main/java/by/tms/insta/storage/JDBCPostStorage.java
@@ -3,6 +3,8 @@ package by.tms.insta.storage;
 import by.tms.insta.entity.Post;
 
 import java.sql.*;
+import java.util.List;
+import java.util.Optional;
 
 public class JDBCPostStorage implements PostStorage {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
@@ -10,6 +12,8 @@ public class JDBCPostStorage implements PostStorage {
     private static final String PASSWORD = "root";
 
     private static final String INSERTING_POST = "insert into posts values (default, ?, ?, ?, ?)";
+
+    private static final String DELETION_POST_BY_ID = "delete from posts where id = ?";
     private final Connection connection;
 
     public JDBCPostStorage() {
@@ -36,6 +40,22 @@ public class JDBCPostStorage implements PostStorage {
 
     @Override
     public void remove(long id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETION_POST_BY_ID);
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    @Override
+    public Optional<Post> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return null;
     }
 }

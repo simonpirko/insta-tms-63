7c2dfe80e4276a0568d41d2f6585ba4b05bac1ae
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-16 16:32:21 +0300

    Updated AbstractStorage

--- src/main/java/by/tms/insta/storage/AbstractStorage.java
+++ src/main/java/by/tms/insta/storage/AbstractStorage.java
@@ -4,16 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class AbstractStorage {
+public abstract class AbstractStorage {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "root";
-    private Connection connection;
 
-    public Connection getConnection(){
+    public Connection getConnection() {
         try {
-            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            return connection;
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }

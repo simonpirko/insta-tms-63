25de2bf28d092c878088dcfe25e37f9f077097d2
Arina <arina.milyutina.0005@gmail.com>
2023-03-18 23:04:22 +0300

    get method getInstance

--- src/main/java/by/tms/insta/service/PostService.java
+++ src/main/java/by/tms/insta/service/PostService.java
@@ -1,4 +1,22 @@
 package by.tms.insta.service;
+
+import by.tms.insta.storage.JDBCPostStorage;
+import by.tms.insta.storage.PostStorage;
+
 public class PostService {
+    private PostStorage postStorage = JDBCPostStorage.getInstance();
+    private PostService INSTANCE;
+
+    private PostService() {
+
+    }
+
+    public PostService getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PostService();
+        }
+        return INSTANCE;
+    }
+
 
 }

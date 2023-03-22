bb4434c305f2b6359a1292745a102e1c21b53c1c
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-22 19:55:29 +0300

    Added a method findById into the class JDBCCommentStorage

--- src/main/java/by/tms/insta/storage/JDBCCommentStorage.java
+++ src/main/java/by/tms/insta/storage/JDBCCommentStorage.java
@@ -1,23 +1,33 @@
 package by.tms.insta.storage;
 
 import by.tms.insta.entity.Comment;
+import by.tms.insta.entity.User;
+import by.tms.insta.service.UserService;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public class JDBCCommentStorage extends AbstractStorage implements Storage<Comment>{
+public class JDBCCommentStorage extends AbstractStorage implements Storage<Comment> {
     private static final String INSERTING_COMMENT = "insert into comments values (default, ?, ?, ?)";
     private static final String DELETION_COMMENT_BY_ID = "delete from comments where id = ?";
+    private static final String SELECTION_COMMENT_BY_ID = "select * from comments where id = ?";
+    private static final int ID_COLUMN = 1;
+    private static final int BODY_COLUMN = 2;
+    private static final int USER_ID_COLUMN = 3;
+    private static final int CREATE_AT_COLUMN = 4;
     private static JDBCCommentStorage commentStorage;
-    private JDBCCommentStorage(){
+
+    private JDBCCommentStorage() {
 
     }
 
-    public static JDBCCommentStorage getInstance(){
-        if(commentStorage == null){
+    public static JDBCCommentStorage getInstance() {
+        if (commentStorage == null) {
             commentStorage = new JDBCCommentStorage();
         }
         return commentStorage;
@@ -49,6 +59,22 @@ public class JDBCCommentStorage extends AbstractStorage implements Storage<Comme
 
     @Override
     public Optional<Comment> findById(long id) {
+        try {
+            PreparedStatement preparedStatement = getConnection().prepareStatement(SELECTION_COMMENT_BY_ID);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            String body = resultSet.getString(BODY_COLUMN);
+            long userId = resultSet.getLong(USER_ID_COLUMN);
+            LocalDateTime createAt = resultSet.getTimestamp(CREATE_AT_COLUMN).toLocalDateTime();
+            return Optional.of(Comment.builder()
+                    .setId(id)
+                    .setBody(body)
+                    .setAuthor(UserService.getInstance().findUserById(userId).get())
+                    .setCreateAt(createAt)
+                    .build());
+        } catch (SQLException ignored) {
+        }
         return Optional.empty();
     }
 

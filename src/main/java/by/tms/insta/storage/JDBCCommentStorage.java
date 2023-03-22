25c10bf2c14b1af359d88ff72fff44fe844609a5
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-22 19:31:54 +0300

    Added a singleton pattern into the class JDBCCommentStorage

--- src/main/java/by/tms/insta/storage/JDBCCommentStorage.java
+++ src/main/java/by/tms/insta/storage/JDBCCommentStorage.java
@@ -2,10 +2,24 @@ package by.tms.insta.storage;
 
 import by.tms.insta.entity.Comment;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-public class JDBCCommentStorage implements Storage<Comment>{
+public class JDBCCommentStorage extends AbstractStorage implements Storage<Comment>{
+    private static JDBCCommentStorage commentStorage;
+    private JDBCCommentStorage(){
+
+    }
+
+    public static JDBCCommentStorage getInstance(){
+        if(commentStorage == null){
+            commentStorage = new JDBCCommentStorage();
+        }
+        return commentStorage;
+    }
+
     @Override
     public void save(Comment value) {
 

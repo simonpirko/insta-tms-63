216cd78ca9b95a76cd7cdcbe16d79cca49e06142
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-03-23 10:39:10 +0300

    Updated an url filed into the class AbstractStorage

--- src/main/java/by/tms/insta/storage/AbstractStorage.java
+++ src/main/java/by/tms/insta/storage/AbstractStorage.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class AbstractStorage {
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/instagram_bd";
     private static final String USER = "postgres";
     private static final String PASSWORD = "root";
     private final Connection connection;
